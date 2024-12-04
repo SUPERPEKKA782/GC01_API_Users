babc266635b8f57b6301b8145edac4aacc55aacb
SUPERPEKKA <dgordillj@alumnos.unex.es>
2024-12-04 11:56:48 +0100

    SCRUM-223 ST83. Remove this unused import 'com.fasterxml.jackson.annotation.JsonIgnore'.

--- src/main/java/io/swagger/model/Profile.java
+++ src/main/java/io/swagger/model/Profile.java
@@ -1,6 +1,5 @@
 package io.swagger.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
